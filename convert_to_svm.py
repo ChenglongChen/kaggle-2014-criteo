b66cc6eadffb25cc3c8f185e21e315cc01d797f5
Yu-Chin <guestwalk@gmail.com>
2014-06-26 04:17:47 +0800

    .

--- convert_to_svm.py
+++ convert_to_svm.py
@@ -3,7 +3,7 @@
 import argparse, csv, hashlib
 
 parser = argparse.ArgumentParser(description='process some integers')
-parser.add_argument('-n', type=int, nargs=1, default=100, help='set number of bins for hashing trick')
+parser.add_argument('-n', type=int, nargs=1, default=1000000, help='set number of bins for hashing trick')
 parser.add_argument('csv_path', type=str, nargs=1, help='set path to the csv file')
 parser.add_argument('svm_path', type=str, nargs=1, help='set path to the svm file')
 args = parser.parse_args()
@@ -17,18 +17,19 @@ with open(SVM_PATH, 'w') as f:
     for i, row in enumerate(csv.reader(open(CSV_PATH))):
         if i == 0:
             continue
-        output = ''
+        feats = []
         for i, element in enumerate(row):
             if i == 0:
                 continue
             elif i == 1:
-                output += element 
+                label = element 
             elif i <= 14:
                 if element == '':
                    element = '0' 
-                output += ' {0}:{1}'.format(i-1, element)
+                feats.append((i-1, element))
             else:
                 bin = hashstr(str(i)+element)+40 if element != '' else i-1
-                output += ' {0}:1'.format(bin)
-        output += '\n'
-        f.write(output)
+                feats.append((bin, 1))
+        feats.sort()
+        feats = ['{0}:{1}'.format(idx, val) for (idx, val) in feats]
+        f.write(label + ' ' + ' '.join(feats) + '\n')
