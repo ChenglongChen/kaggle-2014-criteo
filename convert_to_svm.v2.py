fdc8209f7ef60f910b1e5dbf7baf1c03656a4932
Yu-Chin <guestwalk@gmail.com>
2014-07-02 04:34:31 +0800

    .

--- convert_to_svm.v2.py
+++ convert_to_svm.v2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, csv, sys
+import argparse, csv, sys, math
 
 from common import *
 
@@ -9,12 +9,9 @@ if len(sys.argv) == 1:
 
 parser = argparse.ArgumentParser(description='convert to svm')
 parser.add_argument('-n', default=10000000, type=int, help='set number of bins for hashing trick')
-parser.add_argument('-i', nargs='+', default=[], type=int, help='set numerical fields that are to be treated as numerical')
-parser.add_argument('--', dest='dummy', action='store_true', help='stop parsing optional arguments')
 parser.add_argument('csv_path', type=str, help='set path to the csv file')
 parser.add_argument('svm_path', type=str, help='set path to the svm file')
 args = vars(parser.parse_args())
-args['i'] = set(args['i'])
 
 max_values = {1: 5775.0, 2: 257675.0, 3: 65535.0, 4: 969.0, 5: 23159456.0, 6: 431037.0, 7: 56311.0, 8: 6047.0, 9: 29019.0, 10: 11.0, 11: 231.0, 12: 4008.0, 13: 7393.0}
 
@@ -25,15 +22,15 @@ with open(args['svm_path'], 'w') as f:
         feats = set()
         label = row[1]
         for i, element in enumerate(row[2:15], start=1):
-            if i in args['i']:
-                if element == '':
-                    continue
-                value = float(element)/max_values[i]
-                if value != 0:
-                    feats.add((i, str(round(value, 5))))
-            else:
-                bin = hashstr(str(i)+str(element), args['n'])+14
-                feats.add((bin, 1))
+            bin = hashstr(str(i)+str(element), args['n'])+14
+            feats.add((bin, 1))
+            if element == '':
+                continue
+            value = float(element)
+            if value < 1:
+                continue
+            value = math.log(float(element))
+            feats.add((i, value))
         for i, element in enumerate(row[15:], start=1):
             bin = hashstr(str(i)+element, args['n'])+14
             feats.add((bin, 1))
