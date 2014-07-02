ebf4e3663bdb744c3f89acfa41e6d42b4ecbd05a
Yu-Chin <guestwalk@gmail.com>
2014-07-03 01:29:52 +0800

    .

--- common.py
+++ common.py
@@ -2,5 +2,10 @@ __all__ = ['hashstr']
 
 import hashlib
 
+def open_skip_first_line(path):
+    f = open(path)
+    next(f)
+    return f
+
 def hashstr(str, nr_bins):
     return int(hashlib.md5(str.encode('utf8')).hexdigest(), 16)%nr_bins
