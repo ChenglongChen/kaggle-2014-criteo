5a306c4e2a7784c49a5057052a5cb0389fada40b
Yu-Chin <guestwalk@gmail.com>
2014-07-06 03:38:17 +0800

    .

--- converter.challenger.py
+++ converter.challenger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, csv, sys, math, itertools, queue
+import argparse, csv, sys, random
 
 from common import *
 
@@ -9,25 +9,25 @@ if len(sys.argv) == 1:
 
 parser = argparse.ArgumentParser(description='convert to svm')
 parser.add_argument('-n', '--nr_bins', default=10000000, type=int, help='set number of bins for hashing trick')
-parser.add_argument('-b', '--window_size', default=100000, type=int, help='set the windows size')
+parser.add_argument('-p', '--acc', default=0.9, type=float, help='acc of the virtual classifier')
 parser.add_argument('csv_path', type=str, help='set path to the csv file')
 parser.add_argument('svm_path', type=str, help='set path to the svm file')
 args = vars(parser.parse_args())
 
-window, head, tail = queue.Queue(), 0, 0
 with open(args['svm_path'], 'w') as f:
     for i, row in enumerate(open_csv_skip_first_line(args['csv_path'])):
         feats = set()
         label = row[1]
+        
+        if label == '0':
+            virtual_label = label if random.random() < args['acc'] else '1'
+        else:
+            virtual_label = label if random.random() < args['acc'] else '0'
 
-        window.put(label)
-        if i > args['window_size']:
-            label1 = window.get()
-            feats.add((args['nr_bins']+1000, float(head-tail)/args['window_size']))
-            if label1 == '1':
-                tail += 1
-        if label == '1':
-            head += 1
+        if virtual_label == '0':
+            feats.add((args['nr_bins']+100, 1))
+        else:
+            feats.add((args['nr_bins']+101, 1))
 
         for i, element in enumerate(row[2:15], start=1):
             bin = hashstr(str(i)+str(element), args['nr_bins'])+1
